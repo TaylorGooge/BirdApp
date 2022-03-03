054fb4a0d0e3636c737a29674275984dda70e5c9
Taylor Googe <taylorgooge@Taylors-MacBook-Pro-2.local>
2022-03-02 23:25:32 -0500

    Fix modal for report missing bird

--- Scripts/showModal.js
+++ Scripts/showModal.js
@@ -1,6 +1,10 @@
 var myModal = document.getElementById('reportBirdModal')
 var myInput = document.getElementById('birdModalTrigger')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
+
+$('myModal').on('shown.bs.modal', function () {
+  $('myInput').trigger('focus')
 })
