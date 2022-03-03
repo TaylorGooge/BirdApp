0d2db41dfa330683580d44b9c909dd1267fc8e8f
Taylor Googe <taylorgooge@Taylors-MacBook-Pro-2.local>
2022-03-03 18:17:58 -0500

    Auth0 proxy

--- app.js
+++ app.js
@@ -46,11 +46,7 @@ app.use(
         baseURL: process.env. baseURL,
         clientID: process.env.clientID,
         issuerBaseURL: process.env.issuerBaseURL,
-        session: {
-            cookie: {
-                domain: '.herokuapp.com'
-            }
-        }
+        appSession: false,
     })
 );
 
